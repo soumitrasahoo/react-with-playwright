5849d98eeca92ee9ee5b5e103227add04ef7eda4
soumitrasahoo <soumitrasahoo87@gmail.com>
2023-02-24 15:21:04 +0530

    added the initial test casses

--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -5,6 +5,37 @@ test.describe("Calculator", () => {
         page.goto('http://localhost:3000/');
     });
 
+    test('Initially First Input field should be empty', async ({ page }) => {
+        const input = page.getByTestId('input1');
+        await expect(input).toHaveValue("");
+    });
+
+    test('Initially Second Input field should be empty', async ({ page }) => {
+        const input = page.getByTestId('input2');
+        await expect(input).toHaveValue("");
+    });
+
+    test('Initially Total Input field should be empty', async ({ page }) => {
+        const input = page.getByTestId('total');
+        await expect(input).toHaveValue("");
+    });
+
+    test('add button should enable if both values are there', async ({ page }) => {
+        const input1 = page.getByTestId('input1');
+        const input2 = page.getByTestId('input2');
+        const btn = page.getByRole('button', { name: '+' });
+        await input1.fill("10");
+        await input2.fill("2");
+        await expect(btn).toBeEnabled();
+    });
+
+    test('Reset button should enable if any one of the value is there', async ({ page }) => {
+        const input1 = page.getByTestId('input1');
+        const btn = page.getByRole('button', { name: 'Reset' });
+        await input1.fill("10");
+        await expect(btn).toBeEnabled();
+    });
+
     test('Should add both number after click on the plus button', async ({ page }) => {
         const input1 = page.getByTestId('input1');
         const input2 = page.getByTestId('input2');
